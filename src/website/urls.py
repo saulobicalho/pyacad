6546ba0330a5d3c2171ccf810f7c5678519c0cb3
unknown <sbicalho86@gmail.com>
2019-08-15 19:30:10 -0300

    retry

--- src/website/urls.py
+++ src/website/urls.py
@@ -1,11 +1,13 @@
 # Importamos a função index() definida no arquivo views.py
 from . import views
+from django.urls import path, re_path
 
 app_name = 'website'
 
 # urlpatterns a lista de roteamentos de URLs para funções/Views
 urlpatterns = [
     # GET /
-    url(r'^$', views.index, name='index'),
-    path('funcionarios/<int:ano>/', views.funcionarios_por_ano)
+    """path('', views.index),
+    path('funcionarios/<int:ano>/', views.funcionarios_por_ano)"""
+    path('lista/', views.lista_funcionarios)
 ]
--- src/website/views.py
+++ src/website/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def lista_funcionarios(request):
+    # Primeiro, buscamos os funcionarios
+    funcionarios = Funcionario.objetos.all()
+
+    # Incluímos no contexto
+    contexto = {
+      'funcionarios': funcionarios
+    }
+
+    # Retornamos o template no qual os funcionários serão dispostos
+    return render(request, "funcionarios.html", contexto)
