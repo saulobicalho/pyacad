3e183e5e7344c6c1019b089e3d234310d5ede358
unknown <sbicalho86@gmail.com>
2019-08-16 11:22:07 -0300

    still dealing w/ Funcionario problem @view

--- src/website/urls.py
+++ src/website/urls.py
@@ -1,13 +1,11 @@
 # Importamos a função index() definida no arquivo views.py
-from . import views
-from django.urls import path, re_path
+from website import views
+from django.urls import path
 
 app_name = 'website'
 
 # urlpatterns a lista de roteamentos de URLs para funções/Views
 urlpatterns = [
     # GET /
-    """path('', views.index),
-    path('funcionarios/<int:ano>/', views.funcionarios_por_ano)"""
-    path('lista/', views.lista_funcionarios)
+    path('/lista', views.lista_funcionarios, name='lista_funcionarios'),
 ]
